da562603f0e24c7a5896b6c7e539031de6c2de43
dillonharlessNHRMC <dillonharless@gmail.com>
2021-01-15 14:54:39 -0500

    restructured with new data

--- transformData.py
+++ transformData.py
@@ -1,7 +1,8 @@
 import pandas as pd 
 
 # read in the data and store in dataframe
-df = pd.read_csv('reports.csv',skiprows=None)
+# df = pd.read_csv('reports.csv',skiprows=None)
+df = pd.read_csv('wblivereports.csv',skiprows=None)
 
 # Set up a dict to be used for addition in the loop
 addition_dict = {
@@ -14,31 +15,31 @@ addition_dict = {
 #     Null represents the current logo in the rating is unfilled, hence it should contribute a 0 to the overall rating
 #     If left as null, pandas will throw a nan error
 for i in range(5):
-    df['fun-factor' + str(i)] = df['fun-factor' + str(i)].fillna("zero")
-    # Also replace all nulls in swell-size and water-surface
+    df['fun-factor-' + str(i)] = df['fun-factor-' + str(i)].fillna("zero")
+    # Also replace all nulls in swell_size and water_surface
     if i == 0:
-            df['swell-size'] = df['swell-size'].fillna("Missing")
-            df['water-surface'] = df['water-surface'].fillna("Missing")
+        df['swell_size'] = df['swell_size'].fillna("Missing")
+        df['water_surface'] = df['water_surface'].fillna("Missing")
 
 
 # Iterate through each row in the dataframe
 for index, row in df.iterrows():
     total_rating = 0
     for i in range(5):
-        total_rating += addition_dict[row['fun-factor' + str(i)]]
+        total_rating += addition_dict[row['fun-factor-' + str(i)]]
     df.at[index, 'total_rating'] = total_rating
     # print("The total_rating for {} on {} is {}".format( row["report-link"],  row["date"], total_rating))
-    print("The total_rating for {} on {} is {}".format( row["report-link"],  row["date"], df.at[index, 'total_rating']))
+    print("The total_rating on {} at {} is {}".format( row["date"],  row["time"], df.at[index, 'total_rating']))
 
     # Strip the HTML from these two elements
-    print("df.at[index, 'swell-size']: " + str(df.at[index, 'swell-size']))
-    print("TYPE OF df.at[index, 'swell-size']: " + str(type(df.at[index, 'swell-size'])))
-    df.at[index, 'swell-size'] = df.at[index, 'swell-size'][28:]
-    df.at[index, 'water-surface'] = df.at[index, 'water-surface'][31:]
+    print("df.at[index, 'swell_size']: " + str(df.at[index, 'swell_size']))
+    print("TYPE OF df.at[index, 'swell_size']: " + str(type(df.at[index, 'swell_size'])))
+    df.at[index, 'swell_size'] = df.at[index, 'swell_size'][28:]
+    df.at[index, 'water_surface'] = df.at[index, 'water_surface'][31:]
 
 # Delete the unnecessary columns
 for i in range(5):
-    del df['fun-factor' + str(i)] 
+    del df['fun-factor-' + str(i)] 
 
 
 # TODO Come up with a conversion factor for the swell size
@@ -67,7 +68,8 @@ for i in range(5):
     #     "Drifty": .5 # This could be added to one of the others, so if a number ends in .5 we know it's a combination of something and Drifty
     # }
 
-df.to_csv('transformed_reports.csv')
+# df.to_csv('transformed_reports.csv')
+df.to_csv('transformed_wblivereports.csv')
 
 
 
